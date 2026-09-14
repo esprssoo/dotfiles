4ed5a2aa81f62e073cf4cf7de14df31d449724d5
Nikos <20658439+esprssoo@users.noreply.github.com>
2026-09-14 19:23:42 +0300

    hypr

--- hypr/config/inputs.lua
+++ hypr/config/inputs.lua
@@ -4,7 +4,8 @@ hl.config({
     input = {
         -- sensitivity = -0.25,
         accel_profile = "flat",
-	kb_options = "caps:escape_shifted_capslock",
+        kb_options = "caps:escape_shifted_capslock,grp:alt_shift_toggle",
+        kb_layout = "us,gr",
     },
     -- Uncomment the section below to enable software cursors; this can help with cursor display or behavior issues
     -- cursor = {
@@ -13,6 +14,6 @@ hl.config({
 })
 
 hl.gesture({ fingers = 4, direction = "horizontal", action = "workspace" })
-hl.gesture({ fingers = 3, direction = "down",       action = "close" })
-hl.gesture({ fingers = 3, direction = "up",         action = "fullscreen" })
-hl.gesture({ fingers = 3, direction = "left",       action = "float" })
+hl.gesture({ fingers = 3, direction = "down", action = "close" })
+hl.gesture({ fingers = 3, direction = "up", action = "fullscreen" })
+hl.gesture({ fingers = 3, direction = "left", action = "float" })
